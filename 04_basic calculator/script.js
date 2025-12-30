06d3d4436fb938b8d16cf7e36e13cf5a8cc97634
Darshbhesaniya <darshbhesaniya8@gmail.com>
2025-12-30 16:05:38 +0530

    calculator js code

--- 04_basic calculator/script.js
+++ 04_basic calculator/script.js
@@ -1,64 +1,46 @@
-// const btn = document.querySelectorAll("button");
-// const inputfield = document.querySelector("#result");
-// inputfield.readOnly = true;
+const btn = document.querySelectorAll("button");
+const inputfield = document.querySelector("#result");
+inputfield.readOnly = true;
 
 
-// btn.forEach(buttons => {
-//     buttons.addEventListener('click', function (e) {
-//         // console.log(e);
-//         // console.log(e.target)
-//         console.log(e.target.innerHTML);
-//         // console.log(e.target.id);
+btn.forEach(buttons => {
+    buttons.addEventListener('click', function (e) {
+        // console.log(e);
+        // console.log(e.target)
+        console.log(e.target.innerHTML);
+        // console.log(e.target.id);
 
-//     const key = e.target.innerHTML;        
+    const key = e.target.innerHTML;        
        
 
-//         if (key === 'C') {
-//          clearResult();
-//         }
-//         else if(key === '='){
-//             calculateResult();
-//         }
-//         else{
-//            appendValue(key);
-//         }
-//     })
+        if (key === 'C') {
+         clearResult();
+        }
+        else if(key === '='){
+            calculateResult();
+        }
+        else{
+           appendValue(key);
+        }
+    })
 
-// });
-
-
-// function clearResult(){
-//     inputfield.value = '';
-// }
-
-// function calculateResult(){
-//    try {
-//     inputfield.value = eval(inputfield.value);
-//    } catch {
-// inputfield.value = 'error';
-//    }
-    
-// }
-// function appendValue(value){
-//     inputfield.value += value;
-// }
+});
 
 
-function mydata(){
-
-
-    return new Promise( (resolve, reject)=>{
-       setTimeout(()=>{
-        const data = { name: "rohit", age: 23};
-        resolve(data);
-       }, 3000);
-    });
+function clearResult(){
+    inputfield.value = '';
 }
 
-mydata()
-    .then((data)=>{
-console.log("data", data);
-    })
-    .catch((error)=>{
-        console.log("error", error);
-    })
+function calculateResult(){
+   try {
+    inputfield.value = eval(inputfield.value);
+   } catch {
+inputfield.value = 'error';
+   }
+    
+}
+function appendValue(value){
+    inputfield.value += value;
+}
+
+
